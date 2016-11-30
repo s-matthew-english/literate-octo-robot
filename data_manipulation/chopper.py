7c8ade6667dae4a0fdb4be4cbad9e8ae901a4062
matthias <s.matthew.english@gmail.com>
2016-11-30 19:51:32 +0100

    organized and added data chopper files

---
+++ data_manipulation/chopper.py
@@ -0,0 +1,66 @@
+import json
+from collections import defaultdict
+from pprint import pprint
+
+with open('data-science.txt') as data_file:
+    data = json.load(data_file)
+
+locations = defaultdict(int)
+
+for item in data['data']:
+    location = item['relationships']['location']['data']['id']
+    locations[location] += 1
+
+pprint(locations)
+
+
+
+
+# print each id that corresponds to a single location
+#
+# for item in data['data']:
+#     location = item['relationships']['location']['data']['id']
+#     locations[location].append(item['id'])
+#
+# pprint(locations)
+
+
+
+
+
+
+
+# Get the location data and id
+#
+# for item in data["data"]:
+#     print(item['id'], item['relationships']['location'])
+
+
+# This prints the job title, just some practice
+#
+# for item in data["data"]:
+#     for job in item["attributes"]["title"].splitlines():
+#         print( job )
+
+
+
+
+
+#######################################################
+#   old file
+#######################################################
+# import json
+# from pprint import pprint
+#
+# with open('data.json') as data_file:
+#     data = json.load(data_file)
+#
+#
+#
+#     for item in data["data"]:
+#         # count up by location
+#         for item['relationships']['location']:
+#
+#         item['id']
+#
+########################################################
